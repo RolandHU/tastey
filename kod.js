71911dd186bbd7b08276f9b87ccc3737c84e12e0
VezRoland <vezrolaa@gmail.com>
2021-06-15 18:50:31 +0200

    Create kod.js

---
+++ kod.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", gorgetes);
+var gomb = document.getElementById("gomb");
+
+function gorgetes() {
+    if (window.scrollY >= 727) {
+        gomb.style.right = "100px";
+        gomb.style.bottom = "100px";
+    } else {
+        gomb.style.right = "unset";
+        gomb.style.bottom = "unset";
+    }
+}
