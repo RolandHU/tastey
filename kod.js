f058053410b3dc80ad21e826b6465c1294cbb378
VezRoland <vezrolaa@gmail.com>
2021-06-23 17:04:40 +0200

    Update kod.js

--- kod.js
+++ kod.js
@@ -1,5 +1,7 @@
 var logo = document.getElementById("logo");
 var gomb = document.getElementById("egomb");
+var ajanlat = document.getElementsByClassName("ajanlat");
+var kosar = 0;
 window.addEventListener("load", meretezes);
 window.addEventListener("resize", meretezes);
 
@@ -11,4 +13,22 @@ function meretezes() {
     } else {
         logo.style.width = szel - 40 + "px";
     }
+    if (szel >= 1250) {
+        for (let i = 0; i < ajanlat.length; i++) {
+            ajanlat[i].style.width = "600px";
+        }
+    } else if (szel >= 900) {
+        for (let i = 0; i < ajanlat.length; i++) {
+            ajanlat[i].style.width = "430px";
+        }
+    } else {
+        for (let i = 0; i < ajanlat.length; i++) {
+            ajanlat[i].style.width = szel - 40 + "px";
+        }
+    }
+}
+
+function kosarba() {
+    kosar = kosar + 1;
+    window.alert("A termék kosárba helyezve. \nA kosár tartalma: " + kosar);
 }
